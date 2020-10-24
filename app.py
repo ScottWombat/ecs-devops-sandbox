42de3e8ff7ff74daeea42aa963955d93ffed13ba
Revit Hathaisatpong <revit.hathaisatpong@dta.gov.au>
2020-10-24 12:56:02 +1100

    committed3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
-    """Reverse and return the provided URIk"""
+    """Reverse and return the provided URIkq"""
     return "".join(reversed(random_string))
 
 if __name__ == '__main__':
